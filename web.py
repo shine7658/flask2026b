49d2b1a8cbcfbd5427d020b36866a1e83e507347
shine7658 <shine941011@gmail.com>
2026-04-09 11:40:42 +0800

    0409c

--- web.py
+++ web.py
@@ -37,8 +37,8 @@ def index():
 def read():
     Result = ""
     db = firestore.client()
-    collection_ref = db.collection("靜宜資管")    
-    docs = collection_ref.get()    
+    collection_ref = db.collection("靜宜資管")
+    docs = collection_ref.order_by("lab",direction = firestore.Query.DESCENDING).get()    
     for doc in docs:         
         Result += str(doc.to_dict()) + "<br>"    
     return Result
