974d21ab40e445b840e94fe16cbbc83668238bb3
shine7658 <shine941011@gmail.com>
2026-05-14 11:41:08 +0800

    webhook4

--- web.py
+++ web.py
@@ -58,7 +58,19 @@ def webhook():
 
     if (action == "rateChoice"):
         rate =  req["queryResult"]["parameters"]["rate"]
-        info = "我是林哲旭開發的機器人,您選擇的電影分級是：" + rate
+        info = "我是林哲旭開發的機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+
+        db = firestore.client()
+        collection_ref = db.collection("電影含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                #result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
+
 
     return make_response(jsonify({"fulfillmentText": info}))
 
